a7752f5ec6e3ceb7c354ef9d9605465457c8cfcb
Quanhui <quanhuiliu8@gmail.com>
2020-11-14 21:40:08 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@
 # INPUT1: input file containing the parameters of the gamma distribution of the generation time (string)
 # INPUT2: input file containing the time series (string) [First colum: date; second colum: number of new cases in that date (i.e., the sum of local and imported cases); third colum: number of new imported cases in that date]
 # OUTPUT: name of the file where the output is printed (string)
-./Rt 1 10000 0.05 Tg_7.5 ./../data/Sichuan_2020-02-29 1>Rt-output
+./Rt 1 10000 0.05 Tg_7.5 ./Sichuan_2020-02-29 1>Rt-output
  
